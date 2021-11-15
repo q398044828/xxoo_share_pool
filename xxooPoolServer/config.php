84d822050efd54efd850a84fd17c7e7b0f9a69f0
q398044828 <1520135899@qq.com>
2021-11-15 23:22:41 +0800

    1. 新增数据库管理工具 2. 优化定向助力顺序为用户填写的顺序

--- xxooPoolServer/config.php
+++ xxooPoolServer/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_URL', 'shareCode.db');
-define('TOKEN_PARAMETER_NAME', 'token');
-define('ENV_RANDOM_NUM',15);//针对环境变量随机获取的条数
-define('MAX_NO_UPDATE_DAY',432000);//互助码上报时，最长不更新CREATE_TIME的时间，单位秒 432000=5天
+const DB_URL = 'shareCode.db';
+const TOKEN_PARAMETER_NAME = 'token';
+const ENV_RANDOM_NUM = 15;//针对环境变量随机获取的条数
+const MAX_NO_UPDATE_DAY = 432000;//互助码上报时，最长不更新CREATE_TIME的时间，单位秒 432000=5天
 
 /**
  * 数据库密码，管理作用,这里需要修改成你自己的
@@ -13,4 +13,4 @@ define('MAX_NO_UPDATE_DAY',432000);//互助码上报时，最长不更新CREATE_
  * username: 不填
  * Password: 填你自己设置的密码
  */
-define('DB_PASSWORD',"TEST_PASS");
+const DB_PASSWORD = "test_pass";
